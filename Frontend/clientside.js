4973dbc5d51cbe3a0013e77b5c952f6d12658285
MuteBard <csevere@brandeis.edu>
2018-05-25 09:24:25 -0400

    sumbit upon Add added, table data added upon search button press, need to fix empty behavior on table reload

--- Frontend/clientside.js
+++ Frontend/clientside.js
@@ -1,33 +1,50 @@
 var isFutureDate = (dateString) => new Date(dateString) > new Date()
-
+// var getData = (string) => alert(string)
 
 
 $(document).ready(() => {
-    $.ajax({
-            type: "GET",
-            url:`http://localhost:8080/getAppointments`,
-            success: data => {
-                console.log(data)
-                data.forEach(appointment => {
-                    $('#box1c').append(`<tr class="box2">
-                                            <td>${appointment.bookdate}</td>
-                                            <td>${appointment.booktime}</td>
-                                            <td>${appointment.description}</td>
-                                        </tr>`)
-                })
-            },
-            failure: err => console.log(err)
-        })
 
+    //make a GET request upon pressing get Search button
+    document.getElementById("search").onclick = () => {
+
+        //empty the data on box2 if something is there
+
+        //make this part work left off
+        if (!$('#box2').on("is:empty")){
+            // $("#box2").empty()
+            console.log("TEst")
+        }
+
+        $.ajax({
+                type: "GET",
+                url:`http://localhost:8080/getAppointments`,
+                success: data => {
+                    console.log(data)
+                    data.forEach(appointment => {
+                        $('#box1c').append(`<tr class="box2" id="box2">
+                                                <td>${appointment.bookdate}</td>
+                                                <td>${appointment.booktime}</td>
+                                                <td>${appointment.description}</td>
+                                            </tr>`)
+                    })
+                },
+                failure: err => console.log(err)
+            })
+    }
+   
     
+    //when the New button is pressed..
     $('#addNew').click(() => {
 
-
+        //Toggle the text inside the New button to Add.
         var currentText = $('#addNew').text();
         $('#addNew').text(
             currentText == "New" ? "Add" : "New");
         
         
+        //Create the cancel button upon toggling New.
+        //when it is pressed again, check the conditional and if false, make the div 
+        //containing the button empty
         $('#box4b').append(
             currentText == "New" 
             ? 
@@ -36,39 +53,43 @@ $(document).ready(() => {
             $('#box4b').empty()
         )
 
+        //Upon pressing the Add button the page reloads
+        $('#addNew').text() == "Add" 
+            ? 
+            //delay was added since it would apply the attribute to quickly
+            setTimeout(() => $('form').attr("onsubmit", "return true") , 2000)
+            : 
+            null
+
+        //Create three input fields below the Add and Cancel buttons that manage
+        //Date, Time and Description
         $('#box3b').append(
             currentText == "New" 
             ? 
-            `<form class="secretbox1a" action="/http://localhost:8080/getAppointments" method="get">
-                <div class="secretbox2a">
-                    <input class="secretbox3a" id="secretbox3a" href="javascript:validate()" type="date" name="Search" placeholder="Date">
-                </div>
-                <div class="secretbox2b">
-                    <input class="secretbox3b"  type="text" name="Search" placeholder="Time">
-                </div>
-                <div class="secretbox2c">
-                    <input class="secretbox3c" type="text" name="Search" placeholder="Desc">
-                </div>
-                <input type="submit" value="Submit">
-            </form>` 
+            `
+            <div class="secretbox2a">
+                <input class="secretbox3a" id="secretbox3a" href="javascript:validate()" type="date" name="Search" placeholder="Date">
+            </div>
+            <div class="secretbox2b">
+                <input class="secretbox3b"  type="text" name="Search" placeholder="Time">
+            </div>
+            <div class="secretbox2c">
+                <input class="secretbox3c" type="text" name="Search" placeholder="Desc">
+            </div>
+            ` 
             : 
             $('#box3b').empty())
     
         });
 
-        // $("#secretbox3a").on(datepicker({
-        //     maxDate: "-1d"
-        //   });
-
-        // $(document).on('focus','#secretbox3a', () => {
-            $('#secretbox3a').datepicker({
-                maxDate: "-1d"
-            });
-        // });
-
+    
+    //When in the future if cancel is clicked on...
     $('#box4b').on("click", () => { 
+        //remove the input fields from div #box3b
         $('#box3b').empty()
+        //remove the cancel button from div #box4b
         $('#box4b').empty()
+        //toggle the Add button text back to New
         var currentText = $('#addNew').text();
         $('#addNew').text(currentText == "New" ? "Add" : "New");
         
